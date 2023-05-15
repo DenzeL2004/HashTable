b648093b7c5670d3722279450d9a3522eca31467
DenzeL2004 <work20022004@mail.ru>
2023-05-15 23:07:58 +0300

    change init capacity

--- src/list/list.cpp
+++ src/list/list.cpp
@@ -666,7 +666,6 @@ size_t ListFindVal (const List *list, const elem_t val)
     {
         cur_str = list->data[cur_ind].val;
 
-
         if (!StrcmpIntrinsic(cur_str, val))                           
         {
             return cur_ind;
