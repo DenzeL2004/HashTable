357b13b3d7c06473a795e7d5ca25dac093f008e7
DenzeL2004 <work20022004@mail.ru>
2023-05-19 09:29:15 +0300

    new-test file

--- test_hash_table/config.h
+++ test_hash_table/config.h
@@ -7,16 +7,16 @@ const char *Name_result_file = "temp/result_distribution.csv";
 
 const char *Test_input_file  = "resource/test.txt";
 
-const size_t Hash_table_capacity = 10000ul;
+const size_t Hash_table_capacity = 10007ul;
 
 const size_t Step_print = 1;
 
-const size_t Count_query = 1000000ul;
+const size_t Count_query = 10 * 1000000ul;
 
 //#define TEST_DISTRIBUTION
 
 #define TEST_FIND
 
-//#define PRINT_TIME
+#define PRINT_TIME
 
 #endif
